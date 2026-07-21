ea192dede776de21cc41914d1a2ceb1a2ba8e115
ceoadebi <ceoadebi@outlook.com>
2026-07-20 21:42:46 -0500

    early testing to make sure can read file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Advent of Code Solutions
 
 This is repo will be the list of solutions to advent of code problems. All of this will be practice for me getting better at different languages. As I come up with different solutions, I will further update this repository. Feel free to reach out and comment on this to help me. 
+
+All problems are located at adventofcode.com
--- aoc2025/aoc2025_day1/Csolution/day1.c
+++ aoc2025/aoc2025_day1/Csolution/day1.c
@@ -1,1 +1,35 @@
 #include <stdio.h>
+#include <unistd.h>
+
+/*
+    - Script must be able to read a txt file. 
+    - pass in parameters from said file.  
+    - Paremeters:
+        direction  
+        magnitude
+    - range of numbers: [0,99]
+    - each time dial lands on 0 count it 
+    - dial starts @ 50
+
+*/ 
+
+int main() {
+    //read the file 
+    FILE* fp = fopen("input.txt", "r");
+
+    // if there is no file 
+    if (fp == NULL) {
+        perror("open");
+        return -1;
+    }
+
+    // declare the line variable and allocate 
+    // memory for each line
+    char line[256];
+    
+    //testing if line is being read correctly
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        printf("%s", line);
+    }
+    fclose(fp);
+}
