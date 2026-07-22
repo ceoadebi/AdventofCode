bdc2878488f7bea10f4d8661bb06ca910ba335b6
ceoadebi <ceoadebi@outlook.com>
2026-07-21 22:33:04 -0500

    solving day 1 problem 2025, issue with getting negative numbers on location

--- aoc2025/aoc2025_day1/Csolution/day1.c
+++ aoc2025/aoc2025_day1/Csolution/day1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
-
+#include <string.h>
+#include <stdlib.h>
 /*
     - Script must be able to read a txt file. 
     - pass in parameters from said file.  
@@ -13,10 +14,18 @@
 
 */ 
 
+int rotateDial(int rotationDirection, int currentDialLocation, int rotateHowFar){
+    return ((rotationDirection * rotateHowFar) + currentDialLocation) % 100; 
+
+}
+
 int main() {
     //read the file 
     FILE* fp = fopen("input.txt", "r");
-
+    int timesHitZero = 0;
+    int dialLocation = 50;
+    int currentDialLocation;
+    int rotDir = 1;
     // if there is no file 
     if (fp == NULL) {
         perror("open");
@@ -29,7 +38,41 @@ int main() {
     
     //testing if line is being read correctly
     while (fgets(line, sizeof(line), fp) != NULL) {
-        printf("%s", line);
+        //printf("%s direction: %c\n", line, line[0]);
+        // get the direction of the turn
+        if (line[0] == 'L') {
+            rotDir = -1;
+        } else if (line[0] == 'R') {
+            rotDir = 1;
+        } else {
+            perror("rotation direction");
+            return -1;
+        }
+        // strip the magnitude from the direction
+        char *slicedLine = &line[1];
+        char *endptr;
+        //printf("%s", slicedLine);
+        long magnitude = strtol(slicedLine, &endptr, 10);
+        // printf("%ld\n", magnitude);
+        currentDialLocation = rotateDial(rotDir,currentDialLocation, magnitude);
+        printf("the current location of the dial is %d\n", currentDialLocation);
+        if (currentDialLocation == 0) {
+            timesHitZero++;
+        }
+
+        /*if (line[0] == 'L') {
+        //  printf("The direction is LEFT.\n");
+            printf("Turn the dial LEFT %ld degrees. \n", magnitude);
+        }
+        if (line[0] == 'R') {
+        //  printf("The direction is RIGHT.\n");
+            printf("Turn the dial RIGHT %ld degrees. \n", magnitude);
+        }
+            */
+        
     }
     fclose(fp);
+    printf("the password is %d", timesHitZero);
 }
+
+
